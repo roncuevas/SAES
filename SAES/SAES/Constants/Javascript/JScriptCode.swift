13a83b22d39cf13ac2d507ac0b3e0b78b2907103
Aarón Martínez Cuevas <github@roncuevas.com>
2024-07-18 15:34:36 -0600

    refactor: Architecture modified

---
+++ SAES/SAES/Constants/Javascript/JScriptCode.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+enum JScriptCode {
+    case common
+    case getCaptchaImage
+    case loginForm(String, String, String)
+    case isLogged
+    case reloadCaptcha
+    case logout
+    case personalData
+    case isErrorPage
+    case getProfileImage
+    case schedule
+    case grades
+    
+    var rawValue: String {
+        switch self {
+        case .common:
+            JavaScriptConstants.common
+        case .getCaptchaImage:
+            JavaScriptConstants.getCaptchaImage
+        case .loginForm(let boleta, let password, let captcha):
+            JavaScriptConstants.loginForm(boleta: boleta, password: password, captcha: captcha)
+        case .isLogged:
+            JavaScriptConstants.isLogged
+        case .reloadCaptcha:
+            JavaScriptConstants.reloadCaptcha
+        case .logout:
+            JavaScriptConstants.logout
+        case .personalData:
+            JavaScriptConstants.personalData
+        case .isErrorPage:
+            JavaScriptConstants.isErrorPage
+        case .getProfileImage:
+            JavaScriptConstants.getProfileImage
+        case .schedule:
+            JavaScriptConstants.schedule
+        case .grades:
+            JavaScriptConstants.grades
+        }
+    }
+}
--- SAES/SAES/Constants/Javascript/JavaScriptConstants.swift
+++ SAES/SAES/Constants/Javascript/JavaScriptConstants.swift
@@ -1,46 +1,5 @@
 import Foundation
 
-enum JScriptCode {
-    case common
-    case getCaptchaImage
-    case loginForm(String, String, String)
-    case isLogged
-    case reloadCaptcha
-    case logout
-    case personalData
-    case isErrorPage
-    case getProfileImage
-    case schedule
-    case grades
-    
-    var rawValue: String {
-        switch self {
-        case .common:
-            JavaScriptConstants.common
-        case .getCaptchaImage:
-            JavaScriptConstants.getCaptchaImage
-        case .loginForm(let boleta, let password, let captcha):
-            JavaScriptConstants.loginForm(boleta: boleta, password: password, captcha: captcha)
-        case .isLogged:
-            JavaScriptConstants.isLogged
-        case .reloadCaptcha:
-            JavaScriptConstants.reloadCaptcha
-        case .logout:
-            JavaScriptConstants.logout
-        case .personalData:
-            JavaScriptConstants.personalData
-        case .isErrorPage:
-            JavaScriptConstants.isErrorPage
-        case .getProfileImage:
-            JavaScriptConstants.getProfileImage
-        case .schedule:
-            JavaScriptConstants.schedule
-        case .grades:
-            JavaScriptConstants.grades
-        }
-    }
-}
-
 struct JavaScriptConstants {
     static var common: String = """
     var dict = {};
