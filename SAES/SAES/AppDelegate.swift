24e3f8ee83d7d473d946c91f9646befb24610775
Aarón Martínez Cuevas <github@roncuevas.com>
2025-04-02 10:44:22 -0600

    fix: Add missing plist and Firebase init on appDelegate

--- SAES/SAES/AppDelegate.swift
+++ SAES/SAES/AppDelegate.swift
@@ -1,13 +1,10 @@
 import Foundation
 import UIKit
-@_exported import PostHog
+import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        let posthogApiKey = ProcessInfo.processInfo.environment["POSTHOG_API_KEY"]!
-        let posthogHost = "https://us.i.posthog.com"
-        let config = PostHogConfig(apiKey: posthogApiKey, host: posthogHost)
-        PostHogSDK.shared.setup(config)
+        FirebaseApp.configure()
         return true
     }
 }
