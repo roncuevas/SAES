289c0d75cdd3958cce08dd95763c8ca47b0487b5
Aarón Martínez Cuevas <github@roncuevas.com>
2025-04-05 21:49:40 -0600

    fix: Fix personalData fetching when restoring cookies session

--- SAES/SAES/Core/WebView/WebViewActions.swift
+++ SAES/SAES/Core/WebView/WebViewActions.swift
@@ -26,7 +26,7 @@ final class WebViewActions {
                              url: URLConstants.personalData.value,
                              javaScript: JScriptCode.personalData.value,
                              iterations: 5,
-                             condition: { !self.webViewMessageHandler.personalData.isEmpty })
+                             condition: { self.webViewMessageHandler.personalData.isEmpty })
         ])
     }
     
