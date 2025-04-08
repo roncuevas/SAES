b4f999aa8265fd30d319e7994e2f2a230d74905c
Aarón Martínez Cuevas <github@roncuevas.com>
2025-04-08 16:27:58 -0600

    feat: Add Crashlytics to SAES target

--- SAES/SAES/AppDelegate.swift
+++ SAES/SAES/AppDelegate.swift
@@ -1,6 +1,6 @@
 import Foundation
 import UIKit
-import FirebaseCore
+import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
