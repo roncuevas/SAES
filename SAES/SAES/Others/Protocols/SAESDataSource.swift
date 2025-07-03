1da1ec6296986989fdb07331bc73e6540fbdb689
Aarón Martínez Cuevas <github@roncuevas.com>
2025-07-03 01:33:57 -0600

    feat: Add async string fetcher to SAESDataSource extension
    Introduced SAESFetcherString(url:) to fetch and return response data as a UTF-8 string, similar to the existing data fetcher but returning a String instead of Data.

--- SAES/SAES/Others/Protocols/SAESDataSource.swift
+++ SAES/SAES/Others/Protocols/SAESDataSource.swift
@@ -12,4 +12,12 @@ extension SAESDataSource {
         request.setValue(cookies, forHTTPHeaderField: "Cookie")
         return try await URLSession.shared.data(for: request).0
     }
+
+    func SAESFetcherString(url: URL) async throws -> String? {
+        var request = URLRequest(url: url)
+        let cookies: String = LocalStorageManager.loadLocalCookies(UserDefaults.schoolCode)
+        request.setValue(cookies, forHTTPHeaderField: "Cookie")
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return String(data: data, encoding: .utf8)
+    }
 }
