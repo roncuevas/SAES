7139d4e35221343168e90c227c1222c09770be46
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-11 14:14:54 -0600

    feat: Add QR code generator view using CoreImage

---
+++ SAES/SAES/Screens/Credential/QRCodeGeneratorView.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+struct QRCodeGeneratorView: View {
+    let data: String
+    let size: CGFloat
+
+    init(data: String, size: CGFloat = 200) {
+        self.data = data
+        self.size = size
+    }
+
+    var body: some View {
+        if let image = generateQRCode(from: data) {
+            Image(uiImage: image)
+                .interpolation(.none)
+                .resizable()
+                .scaledToFit()
+                .frame(width: size, height: size)
+        }
+    }
+
+    private func generateQRCode(from string: String) -> UIImage? {
+        let filter = CIFilter.qrCodeGenerator()
+        filter.message = Data(string.utf8)
+        filter.correctionLevel = "M"
+
+        guard let outputImage = filter.outputImage else { return nil }
+
+        let scale = size / outputImage.extent.size.width
+        let scaledImage = outputImage.transformed(by: CGAffineTransform(scaleX: scale, y: scale))
+
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(scaledImage, from: scaledImage.extent) else { return nil }
+
+        return UIImage(cgImage: cgImage)
+    }
+}
