997d02d37edb883c5f5bb95467ed31c2e596f6bc
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-11 23:55:59 -0600

    feat: Add batch status fetch to ServerStatusService

--- SAES/SAES/Screens/Login/ServerStatusService.swift
+++ SAES/SAES/Screens/Login/ServerStatusService.swift
@@ -4,6 +4,18 @@ enum ServerStatusService {
     private static let nsBaseURL = "https://api.roncuevas.com/saes/ns/status"
     private static let nmsBaseURL = "https://api.roncuevas.com/saes/nms/status"
 
+    static func fetchAllStatuses(for type: SchoolType) async -> [String: Bool] {
+        let urlString = type == .highSchool ? nmsBaseURL : nsBaseURL
+        guard let url = URL(string: urlString) else { return [:] }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let statuses = try JSONDecoder().decode([ServerStatus].self, from: data)
+            return Dictionary(uniqueKeysWithValues: statuses.map { ($0.schoolCode, $0.isOnline) })
+        } catch {
+            return [:]
+        }
+    }
+
     static func fetchStatus(for schoolCode: String) async -> Bool? {
         let code = SchoolCodes(rawValue: schoolCode)
         let isHighSchool = code != nil && HighSchoolConstants.schools[code!] != nil
