4b4620ec239a2cd48a165d6fc3c98d9646b72053
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-24 11:05:29 -0600

    feat: add subject palette colors to asset catalog with dark mode

---
+++ SAES/SAES/Others/Extensions/ColorExtension.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+extension Color {
+    static let subjectPalette: [Color] = [
+        Color(.subjectGreen),
+        Color(.subjectCoral),
+        Color(.subjectBlue),
+        Color(.subjectAmber),
+        Color(.subjectPurple),
+        Color(.subjectOrange),
+        Color(.subjectTeal),
+        Color(.subjectPink),
+    ]
+}
