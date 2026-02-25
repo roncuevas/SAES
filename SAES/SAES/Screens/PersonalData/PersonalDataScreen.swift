27b4b69cf16d0e816f2d433a6ca5b859177637d8
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-25 00:05:42 -0600

    fix: remove unused isRunningPersonalData state

--- SAES/SAES/Screens/PersonalData/PersonalDataScreen.swift
+++ SAES/SAES/Screens/PersonalData/PersonalDataScreen.swift
@@ -5,7 +5,6 @@ import SwiftUI
 struct PersonalDataScreen {
     @EnvironmentObject var router: AppRouter
     @EnvironmentObject var webViewMessageHandler: WebViewHandler
-    @State var isRunningPersonalData: Bool = false
     @State var showProfilePicturePreview: Bool = false
     @ObserveInjection var forceRedraw
     @StateObject var viewModel: PersonalDataViewModel = PersonalDataViewModel()
