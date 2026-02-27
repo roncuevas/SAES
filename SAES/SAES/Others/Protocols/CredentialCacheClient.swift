aa3ffd2e5988e6ef811bf548412d86d2777dafb5
Aarón Martínez Cuevas <github@roncuevas.com>
2026-02-27 12:03:55 -0600

    refactor: add hasCredential convenience method to CredentialCacheClient

--- SAES/SAES/Others/Protocols/CredentialCacheClient.swift
+++ SAES/SAES/Others/Protocols/CredentialCacheClient.swift
@@ -4,4 +4,11 @@ protocol CredentialCacheClient: Sendable {
     func load(_ schoolCode: String) -> CredentialWebData?
     func save(_ schoolCode: String, data: CredentialWebData)
     func delete(_ schoolCode: String)
+    func hasCredential(for schoolCode: String) -> Bool
+}
+
+extension CredentialCacheClient {
+    func hasCredential(for schoolCode: String) -> Bool {
+        load(schoolCode) != nil
+    }
 }
