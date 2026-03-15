566f033741ca25dd407487821dc29385d48f55c3
Aarón Martínez Cuevas <github@roncuevas.com>
2026-03-14 22:51:19 -0600

    feat: add RevenueCat API keys for debug and production

--- SAES/SAES/Constants/Secrets/Secrets.swift
+++ SAES/SAES/Constants/Secrets/Secrets.swift
@@ -2,4 +2,11 @@ import Foundation
 
 struct Secrets {
     static let cryptoKey: String = ""
+    static var revenueCatAPIKey: String {
+        #if DEBUG
+        "test_LPrNjxSdRisrGizyABhnPuftUPk"
+        #else
+        "appl_ezSsmdGCXIkeQKFpAPEXbJAYXOC"
+        #endif
+    }
 }
